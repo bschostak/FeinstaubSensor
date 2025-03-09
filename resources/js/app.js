560eb3ac97e3f585d582d7f7b552a04c2f1f0aaa
bschostak <bartek_sz02@proton.me>
2025-03-09 12:05:31 +0100

    feat(app.js): Year form validation
    Before we run a python script, we need to check if the user input from sensor form is valid. When it is not valid, we should get an error message. In this commit it is now added.
    
    BREAKING CHANGE: Added Year validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,37 @@
-function checkYearProportion(startYear, endYear) {
+let startYear = 0;
+let endYear = 0;
+// let sensorId = "";
+let sensorId = "dht22"; //! Should be deleted when the other sensor is added
+
+function setFormDataFromHtmlDocument() {
+    startYear = document.getElementById("startYear").value;
+    endYear = document.getElementById("endYear").value;
+    // sensorId = document.getElementById("sensorId").value;
+}
+
+function checkYearFormValidity(startYear, endYear) {
     if (startYear > endYear) {
         alert("Start year must be less than or equal to end year.");
-        console.log("Start year must be less than or equal to end year.");
-        return;
+        return false;
+    } else if (startYear == 0 || endYear == 0) {
+        alert("Start year and end year must be set.");
+        return false;
+    } else {
+        return true;
     }
 }
 
-function sendSensorDataForChecking() {
-    const startYear = document.getElementById("startYear").value;
-    const endYear = document.getElementById("endYear").value;
-    // const sensorId = document.getElementById("sensorId").value;
-    const sensorId = "dht22"; //! Should be deleted when the other sensor is added
-
-    checkYearProportion(startYear, endYear);
+function sendSensorDataForSensorAnalyze() {
+    console.log("SEND DATA!");
 }
 
 document.getElementById("submitFormDataButton").addEventListener("click", function () {
-    sendSensorDataForChecking();
+    setFormDataFromHtmlDocument();
+
+    let isYearFormatValid = checkYearFormValidity(startYear, endYear);
+    if (!isYearFormatValid) {
+        return;
+    }
+
+    sendSensorDataForSensorAnalyze();
 });
