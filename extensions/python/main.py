00a96f4207a4161ed02e29ece99e2da6a6c3e4ae
bschostak <bartek_sz@protom.me>
2025-03-11 13:49:00 +0100

    style(main.py): Cleaning
    Commented out code is now removed.

--- extensions/python/main.py
+++ extensions/python/main.py
@@ -49,15 +49,7 @@ def processAppEvent(d):
             ext.runThread(taskLongRun, 'taskLongRun', d)
 
 
-# start_year = int(input("Geben Sie den Startjahr ein (default=2024): ").strip() or "2024")
-# end_year = int(input("Geben Sie den Endjahr ein (default=2024): ").strip() or "2024")
-# sensor_type = str("dht22")
-# sensor_id = str(input("Geben Sie die Sensor-ID ein (default=63047): ").strip() or "63047")
-
-# [0] = Datum, [1] = Durchschnittstemperatur, [2] = Höchsttemperatur, [3] = Tiefstemperatur, [4] = Temperaturdifferenz
-# analysed_data = analyze_sensor(start_year, end_year, sensor_type, sensor_id)
-
-# draw_graph(analysed_data)
+#* Application Code (wrapper functions)
 
 def analyze_sensor_wrapper(start_year: int, end_year: int, sensor_type: str, sensor_id: str):
     result = analyze_sensor(start_year, end_year, sensor_type, sensor_id)
